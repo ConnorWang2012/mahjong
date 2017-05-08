df2f4f28d369b309020738fd2d9635bf3bbb680b
ConnorWang2012 <993027682@qq.com>
2017-05-08 17:05:37 +0800

    change proj dir

---
+++ trunk/customer/src/cpp/logic/constant/event_constants.h
@@ -0,0 +1,30 @@
+/*******************************************************************************
+  @ copyright(C), 2015-2020, ConnorAndHisFriendsCompany.Inc
+  @ filename:	  event_constant.h
+  @ author:		  Connor
+  @ version:	  1.0.0
+  @ date:		  2015-01-18
+  @ description:  define all events id here.
+  @ others:  
+  @ history: 
+     1.date:
+       author:
+       modification:
+********************************************************************************/
+
+#ifndef CONNOR_GAME_SRC_EVENT_DEFINE_H_
+#define CONNOR_GAME_SRC_EVENT_DEFINE_H_
+
+namespace gamer 
+{
+
+enum class EventIDs 
+{
+	EVENT_ID_INVALID,
+	EVENT_ID_LOGIN_SUCCESS,
+	EVENT_ID_LOGIN_FAILED
+};
+
+} // namespace gamer
+
+#endif // CONNOR_GAME_SRC_EVENT_DEFINE_H_
---
+++ trunk/customer/src/cpp/logic/manager/lua_bind_helper.h
@@ -0,0 +1,71 @@
+/*******************************************************************************
+  @ copyright(C), 2015-2020, ConnorAndHisFriendsCompany.Inc
+  @ filename:	  lua_bind_helper.cc
+  @ author:		  Connor
+  @ version:	  1.0.0
+  @ date:		  2015-02-19
+  @ description:  offer function to help to bind c++ to lua.
+  @ others:  
+  @ history: 
+     1.date:
+       author:
+       modification:
+********************************************************************************/
+
+#ifndef CONNOR_GAME_SRC_LUA_BIND_HELPER_H_
+#define CONNOR_GAME_SRC_LUA_BIND_HELPER_H_
+
+#include <string>
+extern "C" 
+{
+    #include "lua.h"
+    #include "lualib.h"
+    #include "lauxlib.h"
+}
+
+namespace gamer {
+
+class Event;
+
+class LuaBindHelper {
+  public:
+    virtual ~LuaBindHelper();
+
+    static LuaBindHelper* getInstance();
+
+    static void destoryInstance();
+
+    void setLuaState(lua_State* L);
+
+    // @return  :   if store success, return lua function id, else return empty string.
+    const std::string storeLuaFunction(lua_State* L, int idx);
+
+    void dispatchEvent(const std::string& event_listener_id, Event* event);
+
+    inline lua_State* lua_state() {
+        return lua_state_;
+    }
+
+  private:
+    LuaBindHelper();
+
+    bool init(lua_State* L);
+
+    void closeLuaState();
+
+    std::string createLuaFunctionID();    
+
+    void pushEvent(Event* event);
+
+    bool getLuaFunction(const std::string& function_id);
+
+    void callLuaFunction(int nargs, int nresults);
+
+    //LuaBindHelper* s_lua_bind_helper_;
+    lua_State* lua_state_;
+    int lua_function_id_;
+};
+
+} // namespace gamer
+
+#endif // CONNOR_GAME_SRC_LUA_BIND_HELPER_H_
